3d44d356514e17b1363f7f044f0014203da1715a
jleva <Sdisjkdsakld@ajdshfkjasdhf>
2022-05-13 13:52:02 -0300

    Update

--- interview-question/contador.ocorrencia.js
+++ interview-question/contador.ocorrencia.js
@@ -4,4 +4,8 @@ const frequencies = (arr) =>
         return a
     }, {})
 
-console.log(frequencies(['a', 'b', 'b', 'f', 'b', 'b', 'w'])) // { a: 1, b: 4, f: 1, w: 1 }
+console.log(frequencies(['a', 'b', 'b', 'f', 'b', 'b', 'w']))
+// { a: 1, b: 4, f: 1, w: 1 }
+
+console.log(frequencies([...'contador de ocorrencia']))
+// { c: 3, o: 4, n: 2, t: 1, a: 2, d: 2, r: 3, ' ': 2, e: 2, i: 1 }
